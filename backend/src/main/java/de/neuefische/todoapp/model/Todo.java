4e74fee380fd55a0aa5aa48bf49172d94c0d9526
khuth <kathahuth@yahoo.de>
2021-02-17 14:30:35 +0100

    initial commit

---
+++ backend/src/main/java/de/neuefische/todoapp/model/Todo.java
@@ -0,0 +1,17 @@
+package de.neuefische.todoapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Todo {
+
+    private String id;
+    private String description;
+    private Status status;
+    private String details;
+
+}
